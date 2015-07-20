b5d1e742b1f38b22d17605d473351128a4956a04
Jason Keene <jasonkeene@gmail.com>
2015-07-20 10:40:41 -0400

    Flannel now works and getting kube bins downloaded.

--- terraform/access.tf
+++ terraform/access.tf
@@ -18,4 +18,10 @@ resource "openstack_compute_secgroup_v2" "kuberdemo" {
         ip_protocol = "tcp"
         cidr = "0.0.0.0/0"
     }
+    rule {
+        from_port = "1"
+        to_port = "65535"
+        ip_protocol = "udp"
+        cidr = "0.0.0.0/0"
+    }
 }
